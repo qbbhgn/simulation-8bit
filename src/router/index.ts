fdb0096354e519b229abe0b52b471437581cad9e
田文 <tw704216987@163.com>
2023-08-31 18:21:04 +0800

    跳转到index.html测试成功

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,7 +14,7 @@ const routes: Array<RouteRecordRaw> = [
         component: () => import('../views/main/main.vue'),
       }
     ]
-  },
+  }
 ]
 
 const router = createRouter({
--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,5 +1,38 @@
 <template>
-    <div>
-        <el-button>111</el-button>
+  <div class="main">
+    <div class="header">
+      <el-carousel :interval="4000" type="card" height="250px">
+        <el-carousel-item v-for="item in 6" :key="item" @click="openGame('1')">
+          <h3 text="2xl" justify="center">{{ item }}</h3>
+        </el-carousel-item>
+      </el-carousel>
     </div>
+  </div>
 </template>
+<script lang="ts" setup>
+const openGame = (id:string) =>{
+    const externalLink = "../../../public/games/2048/index.html";
+    window.open(externalLink, "_blank");
+}
+</script>
+<style lang="less">
+.main {
+  width: 100%;
+
+  .el-carousel__item h3 {
+    color: #475669;
+    opacity: 0.75;
+    line-height: 200px;
+    margin: 0;
+    text-align: center;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n + 1) {
+    background-color: #d3dce6;
+  }
+}
+</style>
