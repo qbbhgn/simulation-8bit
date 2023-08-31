f65570556d6d7c63755f75f1386b3d9e059e582d
田文 <tw704216987@163.com>
2023-08-31 15:17:11 +0800

    默认跳转到主页

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,24 +3,32 @@ import type { RouteRecordRaw } from 'vue-router'
 import Home from '../menu/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
-    {
-        path: '/',
-        name: 'Home',
-        component: Home,
-        children:[
-          {
-            path: '/main',
-            name: 'main',
-            component: () => import('../views/main/main.vue'),
-          }
-        ]
-      },
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+    children: [
+      {
+        path: '/main',
+        name: 'main',
+        component: () => import('../views/main/main.vue'),
+      }
+    ]
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
+
+router.beforeEach((to, from, next) => {
+  console.log("路由信息", to, from);
+  if (to.path === '/') {
+    router.push('/main')
+  }
+  next();
+});
 
 export default router
 
